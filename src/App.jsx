8a73f0cd484bc1e8cc922124b7675e1530e48da1
Pedro J Ramirez Jr <plushyhornet189@gmail.com>
2023-10-06 19:41:12 -0700

    Hello there

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import CharacterList from './Components/CharacterList';
+
+function App() {
+  return(
+    <div>
+      <h1 className='title'>The Rick and Morty App</h1>
+
+      <CharacterList/>
+    </div>
+  );
+}
+
+export default App;
